f65b91c973a4986afd41888df1173f1cc51298c6
renan ribeiro <renan.lagee@gmail.com>
2022-06-19 12:55:55 -0300

    att

--- socialbot/settings.py
+++ socialbot/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-l4nf8z-dt$)oi09zq8i393u-^ke6ht8yrqz2-(i#@b2%-xle$0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['44.204.233.152', '172.31.81.239', 'ec2-44-204-233-152.compute-1.amazonaws.com', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
