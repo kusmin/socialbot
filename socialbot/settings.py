4c3b3fbc9ccaecd38a087206138568f78f307f22
renan ribeiro <renan.lagee@gmail.com>
2022-06-19 09:06:30 -0300

    att

--- socialbot/settings.py
+++ socialbot/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-l4nf8z-dt$)oi09zq8i393u-^ke6ht8yrqz2-(i#@b2%-xle$0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['44.204.233.152', '172.31.81.239','ec2-44-204-233-152.compute-1.amazonaws.com' ]
 
 
 # Application definition
@@ -131,3 +132,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
